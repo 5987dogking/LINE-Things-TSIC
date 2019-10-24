950d0a0dbe7eb1332dd39b6f1d782f599d1f4767
Mark <q11295@gmail.com>
2019-10-24 21:52:51 +0800

    mk

--- liff.js
+++ liff.js
@@ -130,7 +130,7 @@ function initializeApp() {
             statusMessage: "􀠁􀅶Quotation marks left􏿿 I'm a Hello World Engineer􀠁􀅷Quotation marks right􏿿",
         };
     }
-    liff.init(({ liffId: window.app.liffId }), initializeLiff(), error => uiStatusError(makeErrorMsg(error), false));
+    liff.init({ liffId: window.app.liffId }, initializeLiff(), error => uiStatusError(makeErrorMsg(error), false));
 }
 
 function initializeLiff() {
